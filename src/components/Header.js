34b116a55a8c54b3878a86d88702dc82f4e97b4b
Pooja Kotak <poojakotak@Poojas-MacBook-Pro.local>
2023-12-15 15:44:04 -0500

    remove header from mobile screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,14 @@
 import React from 'react';
 import '../styles/Header.css';
 
-
 function Header() {
   return (
-    <header className="md:w-1/4 w-full md:h-screen h-auto bg-gray-800 text-white fixed md:flex-col flex-row">
+    <header className="hidden md:block md:w-1/4 h-screen bg-gray-800 text-white fixed">
       <div className="flex flex-col h-full">
         <div className="text-center p-4">
         </div>
-        <nav className="flex-grow flex md:flex-col flex-row justify-center items-center">
-          <ul className="space-y-4 md:text-center text-left md:space-y-4 space-x-2">  
+        <nav className="flex-grow flex flex-col justify-center items-center">
+          <ul className="space-y-4 text-center ">
             <li>
               <a href="#about" className="text-lg hover:text-gray-300">About Me</a>
             </li>
@@ -29,15 +28,5 @@ function Header() {
     </header>
   );
 }
-
-document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href');
-    document.querySelector(targetId).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
 
 export default Header;
