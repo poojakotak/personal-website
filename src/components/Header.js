4b40b930c7e36d53d188f38075d81aa69633fc89
Pooja Kotak <poojakotak@Poojas-MacBook-Pro.local>
2023-12-15 15:32:04 -0500

    remove header from mobile screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,42 +2,31 @@
 import React from 'react';
 import '../styles/Header.css';
 
-  function Header() {
-    return (
-      <header className="w-1/4 h-screen bg-gray-800 text-white fixed">
-        <div className="flex flex-col h-full">
-          <div className="text-center p-4">
-          </div>
-          <nav className="flex-grow flex flex-col justify-center items-center">
-            <ul className="space-y-4 text-center ">
-              <li>
-                <a href="#about" className="text-lg hover:text-gray-300">About Me</a>
-              </li>
-              <li>
-                <a href="#resume" className="text-lg hover:text-gray-300">Resume</a>
-              </li>
-              <li>
-                <a href="#portfolio" className="text-lg hover:text-gray-300">Portfolio</a>
-              </li>
-              <li>
-                <a href="#contact" className="text-lg hover:text-gray-300">Contact Me</a>
-              </li>
-            </ul>
-          </nav>
+function Header() {
+  return (
+    <header className="hidden md:block md:w-1/4 h-screen bg-gray-800 text-white fixed">
+      <div className="flex flex-col h-full">
+        <div className="text-center p-4">
         </div>
-      </header>
-    );
-  }
-  
-
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href');
-      document.querySelector(targetId).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
+        <nav className="flex-grow flex flex-col justify-center items-center">
+          <ul className="space-y-4 text-center ">
+            <li>
+              <a href="#about" className="text-lg hover:text-gray-300">About Me</a>
+            </li>
+            <li>
+              <a href="#resume" className="text-lg hover:text-gray-300">Resume</a>
+            </li>
+            <li>
+              <a href="#portfolio" className="text-lg hover:text-gray-300">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contact" className="text-lg hover:text-gray-300">Contact Me</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
