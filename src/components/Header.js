a71363110edf1ef2d56c98208a9252e183a20b54
Pooja Kotak <poojakotak@Poojas-MacBook-Pro.local>
2023-12-15 15:26:31 -0500

    mobile header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,39 +3,36 @@ import React, { useState } from 'react';
 import '../styles/Header.css';
 
 function Header() {
-  const [isNavVisible, setIsNavVisible] = useState(false);
-
-  const toggleNav = () => {
-    setIsNavVisible(!isNavVisible);
-  };
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <header className="w-full md:w-1/4 h-screen bg-gray-800 text-white fixed">
-      <div className="flex flex-col h-full">
-        {/* Hamburger Menu Icon */}
-        <div className="text-center p-4 md:hidden">
-          <button onClick={toggleNav}>
-            <i className="fas fa-bars"></i> {/* Font Awesome icon, ensure you include Font Awesome or use an alternative */}
-          </button>
-        </div>
-        {/* Navigation Links */}
-        <nav className={`flex-grow ${isNavVisible ? 'flex' : 'hidden'} md:flex flex-col justify-center items-center`}>
-          <ul className="space-y-4 text-center">
-            <li>
-              <a href="#about" className="text-lg hover:text-gray-300">About Me</a>
-            </li>
-            <li>
-              <a href="#resume" className="text-lg hover:text-gray-300">Resume</a>
-            </li>
-            <li>
-              <a href="#portfolio" className="text-lg hover:text-gray-300">Portfolio</a>
-            </li>
-            <li>
-              <a href="#contact" className="text-lg hover:text-gray-300">Contact Me</a>
-            </li>
-          </ul>
-        </nav>
+    <header className="bg-gray-800 text-white fixed h-screen w-full md:w-1/4">
+      {/* Hamburger Menu Icon */}
+      <div className="text-center p-4 md:hidden">
+        <button onClick={() => setIsNavOpen(!isNavOpen)}>
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+          </svg>
+        </button>
       </div>
+
+      {/* Navigation Links */}
+      <nav className={`flex-grow ${isNavOpen ? 'flex' : 'hidden'} flex-col justify-center items-center md:flex h-full`}>
+        <ul className="space-y-4 text-center">
+          <li>
+            <a href="#about" className="text-lg hover:text-gray-300">About Me</a>
+          </li>
+          <li>
+            <a href="#resume" className="text-lg hover:text-gray-300">Resume</a>
+          </li>
+          <li>
+            <a href="#portfolio" className="text-lg hover:text-gray-300">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact" className="text-lg hover:text-gray-300">Contact Me</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
