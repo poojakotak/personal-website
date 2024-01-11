c735206468b48907c278e5917985ae279f6d6ffe
Pooja Kotak <poojakotak1999@gmail.com>
2024-01-11 13:31:20 -0500

    test navbar on mobile screens

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About() {
         <div className="max-w-2xl mx-auto p-4 text-center">
           <img src={placeholderImage} alt="Pooja Kotak" className="rounded-full mx-auto mb-4 w-40 h-40 object-cover" />
           <h2 className="text-5xl font-bold mb-3 text-gray-800">Hi, I'm Pooja!</h2>
-          <p className="mb-2 text-l font-bold text-gray-800">I love to gain technical knowlegde and build 0 to 1.</p>
+          <p className="mb-2 text-l font-bold text-gray-800">I love to learn about tech and build 0 to 1.</p>
           <p className="mb-5">Over the past few months, I’ve enhanced my product sense through self-driven projects, interviewing for various positions, and studying the evolution and pitfalls of current tech products. Having led cross-functional teams and taking initiatives for product growth at Microsoft, I gained a unique perspective on product strategy and user experience. Discover more about my personal projects and interests below.</p>
         </div>
       </section>
